91922bd75d1efd3cfc561456720ec324d4809d5d
Michael Handler <michael.handler@fullstop.at>
2010-10-20 12:35:32 +0200

    Start implementing project manager.
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

---
+++ core/projectmanager/src/main/java/org/openengsb/opencit/core/projectmanager/internal/ProjectManagerImpl.java
@@ -0,0 +1,38 @@
+/**
+ * Copyright 2010 OpenEngSB Division, Vienna University of Technology
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.openengsb.opencit.core.projectmanager.internal;
+
+import java.util.List;
+
+import org.openengsb.opencit.core.projectmanager.ProjectManager;
+import org.openengsb.opencit.core.projectmanager.model.Project;
+
+public class ProjectManagerImpl implements ProjectManager {
+
+    @Override
+    public void createProject(Project project) {
+        // TODO Auto-generated method stub
+
+    }
+
+    @Override
+    public List<Project> getAllProjects() {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+}
