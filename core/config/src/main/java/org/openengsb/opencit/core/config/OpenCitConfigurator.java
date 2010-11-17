5def06a78a9df1ee1fe5aa51f4b149c3cd5f313f
Michael Handler <michael.handler@fullstop.at>
2010-11-17 11:57:42 +0100

    OPENCIT-8 only add imports and globals, rules and workflow if they are not already present.
    If they are present, they are updated.
    
    Signed-off-by: Michael Handler <michael.handler@fullstop.at>

--- core/config/src/main/java/org/openengsb/opencit/core/config/OpenCitConfigurator.java
+++ core/config/src/main/java/org/openengsb/opencit/core/config/OpenCitConfigurator.java
@@ -18,7 +18,9 @@ package org.openengsb.opencit.core.config;
 
 import java.io.InputStream;
 import java.util.Arrays;
+import java.util.Collection;
 import java.util.List;
+import java.util.Map;
 import java.util.UUID;
 
 import org.apache.commons.io.IOUtils;
@@ -78,28 +80,28 @@ public class OpenCitConfigurator {
 
     private void addScmGlobalsAndImports() throws RuleBaseException {
         ruleManager.addImport(ScmDomain.class.getCanonicalName());
-        ruleManager.addGlobal(ScmDomain.class.getCanonicalName(), "scm");
+        addGlobal(ScmDomain.class.getCanonicalName(), "scm");
     }
 
     private void addBuildGlobalsAndImports() throws RuleBaseException {
         ruleManager.addImport(BuildStartEvent.class.getCanonicalName());
         ruleManager.addImport(BuildEndEvent.class.getCanonicalName());
         ruleManager.addImport(BuildDomain.class.getCanonicalName());
-        ruleManager.addGlobal(BuildDomain.class.getCanonicalName(), "build");
+        addGlobal(BuildDomain.class.getCanonicalName(), "build");
     }
 
     private void addTestGlobalsAndImports() throws RuleBaseException {
         ruleManager.addImport(TestStartEvent.class.getCanonicalName());
         ruleManager.addImport(TestEndEvent.class.getCanonicalName());
         ruleManager.addImport(TestDomain.class.getCanonicalName());
-        ruleManager.addGlobal(TestDomain.class.getCanonicalName(), "test");
+        addGlobal(TestDomain.class.getCanonicalName(), "test");
     }
 
     private void addDeployGlobalsAndImports() throws RuleBaseException {
         ruleManager.addImport(DeployStartEvent.class.getCanonicalName());
         ruleManager.addImport(DeployEndEvent.class.getCanonicalName());
         ruleManager.addImport(DeployDomain.class.getCanonicalName());
-        ruleManager.addGlobal(DeployDomain.class.getCanonicalName(), "deploy");
+        addGlobal(DeployDomain.class.getCanonicalName(), "deploy");
     }
 
     private void addReportGlobalsAndImports() throws RuleBaseException {
@@ -107,21 +109,21 @@ public class OpenCitConfigurator {
         ruleManager.addImport(Report.class.getCanonicalName());
         ruleManager.addImport(ReportPart.class.getCanonicalName());
         ruleManager.addImport(NoSuchReportException.class.getCanonicalName());
-        ruleManager.addGlobal(ReportDomain.class.getCanonicalName(), "report");
+        addGlobal(ReportDomain.class.getCanonicalName(), "report");
     }
 
     private void addNotificationGlobalsAndImports() throws RuleBaseException {
         ruleManager.addImport(NotificationDomain.class.getCanonicalName());
         ruleManager.addImport(Notification.class.getCanonicalName());
         ruleManager.addImport(Attachment.class.getCanonicalName());
-        ruleManager.addGlobal(NotificationDomain.class.getCanonicalName(), "notification");
+        addGlobal(NotificationDomain.class.getCanonicalName(), "notification");
     }
 
     private void addProjectManagerGlobalsAndImports() throws RuleBaseException {
         ruleManager.addImport(ProjectManager.class.getCanonicalName());
         ruleManager.addImport(Project.class.getCanonicalName());
         ruleManager.addImport(State.class.getCanonicalName());
-        ruleManager.addGlobal(ProjectManager.class.getCanonicalName(), "projectManager");
+        addGlobal(ProjectManager.class.getCanonicalName(), "projectManager");
     }
 
     private void addWorkflow() {
@@ -130,12 +132,26 @@ public class OpenCitConfigurator {
             is = getClass().getClassLoader().getResourceAsStream("ci.rf");
             String citWorkflow = IOUtils.toString(is);
             RuleBaseElementId id = new RuleBaseElementId(RuleBaseElementType.Process, "ci");
-            ruleManager.add(id, citWorkflow);
+            if (isPresent(id)) {
+                ruleManager.update(id, citWorkflow);
+            } else {
+                ruleManager.add(id, citWorkflow);
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
             IOUtils.closeQuietly(is);
         }
+    }
+
+    private boolean isPresent(RuleBaseElementId id) {
+        Collection<RuleBaseElementId> elements = ruleManager.list(id.getType());
+        for (RuleBaseElementId element : elements) {
+            if (element.equals(id)) {
+                return true;
+            }
+        }
+        return false;
     }
 
     private void addRules() {
@@ -153,12 +169,28 @@ public class OpenCitConfigurator {
             is = getClass().getClassLoader().getResourceAsStream(rule + ".rule");
             String ruleText = IOUtils.toString(is);
             RuleBaseElementId id = new RuleBaseElementId(RuleBaseElementType.Rule, rule);
-            ruleManager.add(id, ruleText);
+            if (isPresent(id)) {
+                ruleManager.update(id, ruleText);
+            } else {
+                ruleManager.add(id, ruleText);
+            }
         } catch (Exception e) {
             throw new RuntimeException(e);
         } finally {
             IOUtils.closeQuietly(is);
         }
+    }
+
+    private void addGlobal(String clazz, String name) throws RuleBaseException {
+        if (isGlobalPresent(name)) {
+            ruleManager.removeGlobal(name);
+        }
+        ruleManager.addGlobal(clazz, name);
+    }
+
+    private boolean isGlobalPresent(String global) {
+        Map<String, String> globals = ruleManager.listGlobals();
+        return globals.containsKey(global);
     }
 
     public void setRuleManager(RuleManager ruleManager) {
